80f3e59e8a085646f42f6d27f962e968a5d30584
Liam Kelly <liam.james.kelly@gmail.com>
2022-01-22 09:40:35 +0000

    refactored 2021, day7

--- advent/y2021/d7.py
+++ advent/y2021/d7.py
@@ -4,18 +4,27 @@ from math import ceil, floor
 from statistics import median, mean
 
 
-def cost(crabs_pos: list[int], mean_pos: int) -> int:
-    fuel = [abs(pos - mean_pos) for pos in crabs_pos]
-    adj = []
-    while len(fuel) >= 1:
-        adj.append(sum(x for x in range(1, fuel.pop()+1)))
-    return sum(adj)
+def abs_diff(a, b) -> int:
+    return int(abs(a - b))
+
+
+def sum_to(n: int) -> int:
+    return int((n * (n + 1)) / 2)
+
+
+def median_cost(crabs_pos) -> int:
+    med_pos = int(median(crabs_pos))
+    return sum([abs_diff(pos, med_pos) for pos in crabs_pos])
+
+
+def mean_cost(crabs_pos) -> int:
+    mean_pos = mean(crabs_pos)
+    return  min(
+        sum([sum_to(abs_diff(pos, floor(mean_pos))) for pos in crabs_pos]),
+        sum([sum_to(abs_diff(pos, ceil(mean_pos))) for pos in crabs_pos])
+    )
 
 
 def solve(input: list[str]) -> Solution:
     crabs_pos = sorted([int(x) for x in input[0].split(",")])
-    mean_pos = mean(crabs_pos)  # save us calculating the mean position twice
-    return Solution(
-        sum([abs(pos - int(median(crabs_pos))) for pos in crabs_pos]),
-        min(cost(crabs_pos, floor(mean_pos)), cost(crabs_pos, ceil(mean_pos))),
-    )
+    return Solution(median_cost(crabs_pos), mean_cost(crabs_pos))
